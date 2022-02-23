e51f6d768619f44a2d70315f21f44308c1055a0f
Rel-ativity <davkojak@hotmail.com>
2022-02-22 23:40:31 -0500

    23:40

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # C_Learning_WinApi_Windows_Tutorial
 
-Just me trying to learn some basic C 
+Just me trying to learn some basic C
+-------------------------------------- \
+**Im using 'TODO Highlight' extension on VSCODE**
+
+21/2/2022
+    -Watched a quick C Video to get the basic idea
+    -Wanted to make a GUI with DTK, but DTK work much better on Linux
+    -Switched to WIN32api to make a simple window
+
+22/2/2022
+    -Commented a few stuff from the tutorial
+    -Implementing my little library, to make stuff easier in the future
+        -Added r_msgBox - r_msgBox("This is the title", "This is the text")
+            TODO:
+                -Choice for Textbox, Ok/Cancel, goal is to be able to make multiple textbox
+    
+
+
--- main.c
+++ main.c
@@ -1,37 +1,71 @@
 #include <stdio.h>
 #include <windows.h>
 
-LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+// Trying to make my own C Library
+#include "Rellib.h"
 
+
+
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args,int ncmdshow)
 {
-   WNDCLASSW wc = {0};
-   wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-   wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-   wc.hInstance = hInst;
-   wc.lpszClassName = L"MyWindowClass";
-   wc.lpfnWndProc = WindowProcedure;
-   if(!RegisterClassW(&wc))
+   // Trying to implement a way to launch a windows without doing 70 lines of code
+   // Doing it step by step, for learning purposes.
+   int dev = 0;
+   if (dev == 0)
    {
-      return -1;
+      
+      //r_msgBox("Fatal error","Not enough vespene gaz");
+      r_print("va chier criss de cave");
+
    }
-   CreateWindowW(L"MyWindowClass", L"Testing", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 300, 300,NULL, NULL, NULL, NULL);
-   MSG msg = {0};
-   while(GetMessage(&msg, NULL,NULL,NULL))
+   else
    {
-      TranslateMessage(&msg);
-      DispatchMessage(&msg);
+      
+      WNDCLASSW wc = {0};
+
+      //Background colors - Link for info
+      //https://i.imgur.com/ZgexXsH.png
+      wc.hbrBackground = (HBRUSH)COLOR_HIGHLIGHT;
+
+
+      //Icon of the cursor, in the window ( Set to CROSS, for fun )
+      wc.hCursor = LoadCursor(NULL, IDC_CROSS);
+      wc.hInstance = hInst;
+      //Window class name, probably to let win32api control it by its name
+      wc.lpszClassName = L"MyWindowClass";
+
+      wc.lpfnWndProc = WindowProcedure;
+
+      if(!RegisterClassW(&wc))
+      {
+         return -1;
+      }
+
+      // Actual window
+      CreateWindowW(L"MyWindowClass", L"Test Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 300, 300,NULL, NULL, NULL, NULL);
+
+      //Probably some not implemented stuff. Not done with the tutorial
+      MSG msg = {0};
+      while(GetMessage(&msg, NULL,NULL,NULL))
+      {
+         TranslateMessage(&msg);
+         DispatchMessage(&msg);
+      }
+      return 0;
    }
-   return 0;
 }
 
 LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
    switch(msg)
    {
+   //If the user close the window, Break.
    case WM_DESTROY:
       PostQuitMessage(0);
       break;
+   //Otherwise, we re-display the window (?)   
    default:
       return DefWindowProcW(hWnd, msg, wp, lp);
    }
